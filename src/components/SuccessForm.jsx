aec388c7442508cf260bb22b457f09135570c576
DavidOsezua <102161226+DavidOsezua@users.noreply.github.com>
2024-04-18 10:02:33 +0100

    at last

--- src/components/SuccessForm.jsx
+++ src/components/SuccessForm.jsx
@@ -2,16 +2,14 @@ import React from "react";
 import styles from "./FormTwo.module.css";
 import { useModal } from "../pages/walletPage";
 import { NavLink } from "react-router-dom";
+import { check } from "../assets";
 
 const SuccessForm = ({ selectedNetwork }) => {
   const { modalHandler } = useModal();
 
   return (
     <div className="flex flex-col items-center">
-      <p className="text-[0.8rem] text-center my-[0.3rem]">
-        The wallet address you deposited to serves as your referral code. Invite
-        your friend and have them use your wallet address as the referral code
-      </p>
+      <img src={check} />
 
       <h2 className={`${styles.successTitle} mt-[0.7rem]`}>
         Wallet registered succefully
